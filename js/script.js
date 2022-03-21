f8e0e0681b1a30521e2a0425eb1a4fe033ca61fa
pronepoet <ochiengphillip84@gmail.com>
2022-03-21 15:20:03 +0300

    added jquery to show and hide the input field dor destination

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@ $(document).ready(()=>{
     //sees whether a customer wants delivery 
     $(".deliveryYes").click(()=>{
      $(".deliveryAmount").show();
+     $(".pizzaDelivery").show();
     })
     $(".deliveryNo").click(()=>{
         $(".deliveryAmount").hide();
+        $(".pizzaDelivery").hide();
     })
+    
 })
