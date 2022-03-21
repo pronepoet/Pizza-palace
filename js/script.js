91b66864cc3a41e1632d063f99f91fb6c4a283fd
pronepoet <ochiengphillip84@gmail.com>
2022-03-21 15:22:38 +0300

    Ft: add the display area for the order details before a customer places an order

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+//business logic
+let size = document.querySelector(".pizzaSize").value;
+let crust = document.querySelector(".pizzaCrust").value;
+let topping = document.querySelector(".pizzaToppings").value;
+let quantity = document.querySelector(".pizzaQuantity").value;
+let destination = document.querySelector(".pizzaDelivery").value;
+let order = document.querySlelector(".placeOrder");
+
+// user interface logic
 $(document).ready(()=>{
     //sees whether a customer wants delivery 
     $(".deliveryYes").click(()=>{
