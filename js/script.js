fa5701c94c9337ca9c8d3b57d60bce5adf3352cd
pronepoet <ochiengphillip84@gmail.com>
2022-03-31 23:26:36 +0300

    added a container

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+var i = 0; text;
+text = 'Hi there traveller'
+function typing() {
+    if(i<text.length){
+        document.getElementById("text").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typing,50);
+    }
+}
+typing();
+
+
+
+
 function Pizza(sizeP, crustP, toppingP, totalP) {
     this.size = sizeP;
     this.topping = toppingP;
